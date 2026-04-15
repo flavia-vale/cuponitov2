67d073ff77fa1900f43889221ed81c2b9e983f59
[dyad] <flavia.vale@usp.br>
2026-04-15 17:53:47 -0300

    [dyad] Configurando projeto para deploy na Netlify - wrote 2 file(s), deleted 2 file(s)

--- functions/[[path]].ts
+++
@@ -1,9 +0,0 @@
-export const onRequest: PagesFunction = async (context) => {
-  const res = await context.next();
-  // Se o servidor retornar 404 (não achou o arquivo físico), 
-  // servimos o index.html da raiz para o TanStack Router assumir.
-  if (res.status === 404) {
-    return context.env.ASSETS.fetch(new URL("/", context.request.url));
-  }
-  return res;
-};
