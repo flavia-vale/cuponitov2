e195b2654aad317de79bd0fbe6553e164ec9a8e6
[dyad] <flavia.vale@usp.br>
2026-04-15 20:14:36 -0300

    [dyad] Removendo arquivos de config inúteis e aplicando fallback de 404.html para SPA no ambiente interno - wrote 2 file(s), deleted 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from "@/components/ui/sonner";
@@ -31,8 +31,8 @@ export default function App() {
             <Route path="/blog/:slug" element={<BlogPost />} />
             <Route path="/admin" element={<AdminDashboard />} />
             <Route path="/admin/login" element={<AdminLogin />} />
-            {/* Fallback to Home */}
-            <Route path="*" element={<Navigate to="/" replace />} />
+            {/* Rota curinga (Truque do Lovable/Surge) */}
+            <Route path="*" element={<Home />} />
           </Routes>
         </BrowserRouter>
         <Toaster />
