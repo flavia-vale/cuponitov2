f8ebebce942904af109bc3810a52a9b0b0ba0330
[dyad] <flavia.vale@usp.br>
2026-04-14 22:22:18 -0300

    Reverted all changes back to version dbc43d9c264b8047cc11ad1f9abf3d2833e2f791

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import "./styles.css";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
-import Header from './components/Header'; // Novo import
-import HeroBanner from './components/HeroBanner';
 
 const router = getRouter();
 
